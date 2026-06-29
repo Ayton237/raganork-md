05180e710c2670247ab5b4c5145b2b3132c5205a
Ayton237 <111095419+Ayton237@users.noreply.github.com>
2026-06-29 17:10:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const sequelize =
         logging: DEBUG,
       });
 
-const SESSION_STRING = process.env.SESSION || process.env.SESSION_ID;
+const SESSION_STRING = process.env.SESSION || process.env.SESSION_ID; RGNK~UneKvMTX
 
 const SESSION = SESSION_STRING
   ? SESSION_STRING.split(",").map((s) => s.split("~")[1].trim())
